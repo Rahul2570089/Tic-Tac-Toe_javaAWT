616c791a8078d2606444552fc37019f6487725f1
rahul <rs2570089@gmail.com>
2022-10-20 21:34:37 +0530

    Updated

--- src/main/java/Layout.java
+++ src/main/java/Layout.java
@@ -2,30 +2,34 @@ import java.awt.*;
 import java.awt.event.*;
 
 
-public class Layout extends Frame implements  ActionListener {
+public class Layout extends Frame implements  ActionListener{
 
-    Button[] b = new Button[9];
+    Button[][] b1 = new Button[3][3];
     boolean toggle;
+    boolean isWin, allow;
     int numOfClicks=0;
+
+    MyDialog d;
+    Panel p;
+    TextField tf;
     public Layout(String title){
         setTitle(title);
         setLayout(new GridLayout(3,3));
-        for(int i=0;i<9;i++) {
-            add(b[i] = new Button(String.valueOf(i)));
-            b[i].addActionListener(this);
+
+        for (int i = 0; i < 9; i++) {
+            int row = i / 3;
+            int col = i % 3;
+            add(b1[row][col] = new Button());
+            b1[row][col].addActionListener(this);
+            b1[row][col].setFont(new Font("Arial", Font.BOLD, 50));
         }
 
-        setSize(400,400);
+        setSize(600,600);
         setVisible(true);
         addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
-                super.windowClosing(e);
                 System.exit(0);
-            }
-            @Override
-            public void windowClosed(WindowEvent e) {
-                super.windowClosed(e);
             }
         });
         setResizable(false);
@@ -37,153 +41,237 @@ public class Layout extends Frame implements  ActionListener {
 
     @Override
     public void paint(Graphics g) {
-        for(int i=0;i<9;i++) {
-            b[i].setEnabled(true);
-            b[i].setLabel(String.valueOf(i));
+        if(allow) {
+            if(!isWin && numOfClicks==9) {
+                numOfClicks = 0;
+                for(int i=0;i<9;i++) {
+                    int row = i / 3;
+                    int col = i % 3;
+                    b1[row][col].setLabel("");
+                }
+                d = new MyDialog(this, "Draw", true);
+                d.setVisible(true);
+            }
+            else {
+                if(toggle)
+                    d = new MyDialog(this, "O won", true);
+                else
+                    d = new MyDialog(this, "X won", true);
+                numOfClicks = 0;
+                for(int i=0;i<9;i++) {
+                    int row = i / 3;
+                    int col = i % 3;
+                    b1[row][col].setLabel("");
+                }
+                d.setVisible(true);
+            }
         }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         String s = e.getActionCommand();
-        if(s.equals("0") && toggle) {
+
+        if(b1[0][0] == e.getSource() && toggle && b1[0][0].getLabel().equals("")) {
             numOfClicks++;
-            toggle = false;
-            b[0].setLabel("X");
-            b[0].setEnabled(false);
+            toggle = !toggle;
+            b1[0][0].setLabel("X");
         }
-        else if(s.equals("0") && !toggle) {
+        else if(b1[0][0] == e.getSource() && !toggle && b1[0][0].getLabel().equals("")) {
             numOfClicks++;
-            toggle = true;
-            b[0].setLabel("O");
-            b[0].setEnabled(false);
-        }
-        if(s.equals("1") && toggle) {
-            numOfClicks++;
-            toggle = false;
-            b[1].setLabel("X");
-            b[1].setEnabled(false);
-        }
-        else if(s.equals("1") && !toggle) {
-            numOfClicks++;
-            toggle = true;
-            b[1].setLabel("O");
-            b[1].setEnabled(false);
-        }
-        if(s.equals("2") && toggle) {
-            numOfClicks++;
-            toggle = false;
-            b[2].setLabel("X");
-            b[2].setEnabled(false);
-        }
-        else if(s.equals("2") && !toggle) {
-            numOfClicks++;
-            toggle = true;
-            b[2].setLabel("O");
-            b[2].setEnabled(false);
-        }
-        if(s.equals("3") && toggle) {
-            numOfClicks++;
-            toggle = false;
-            b[3].setLabel("X");
-            b[3].setEnabled(false);
-        }
-        else if(s.equals("3") && !toggle) {
-            numOfClicks++;
-            toggle = true;
-            b[3].setLabel("O");
-            b[3].setEnabled(false);
-        }
-        if(s.equals("4") && toggle) {
-            numOfClicks++;
-            toggle = false;
-            b[4].setLabel("X");
-            b[4].setEnabled(false);
-        }
-        else if(s.equals("4") && !toggle) {
-            numOfClicks++;
-            toggle = true;
-            b[4].setLabel("O");
-            b[4].setEnabled(false);
-        }
-        if(s.equals("5") && toggle) {
-            numOfClicks++;
-            toggle = false;
-            b[5].setLabel("X");
-            b[5].setEnabled(false);
-        }
-        else if(s.equals("5") && !toggle) {
-            numOfClicks++;
-            toggle = true;
-            b[5].setLabel("O");
-            b[5].setEnabled(false);
-        }
-        if(s.equals("6") && toggle) {
-            numOfClicks++;
-            toggle = false;
-            b[6].setLabel("X");
-            b[6].setEnabled(false);
-        }
-        else if(s.equals("6") && !toggle) {
-            numOfClicks++;
-            toggle = true;
-            b[6].setLabel("O");
-            b[6].setEnabled(false);
-        }
-        if(s.equals("7") && toggle) {
-            numOfClicks++;
-            toggle = false;
-            b[7].setLabel("X");
-            b[7].setEnabled(false);
-        }
-        else if(s.equals("7") && !toggle) {
-            numOfClicks++;
-            toggle = true;
-            b[7].setLabel("O");
-            b[7].setEnabled(false);
-        }
-        if(s.equals("8") && toggle) {
-            numOfClicks++;
-            toggle = false;
-            b[8].setLabel("X");
-            b[8].setEnabled(false);
-        }
-        else if(s.equals("8") && !toggle) {
-            numOfClicks++;
-            toggle = true;
-            b[8].setLabel("O");
-            b[8].setEnabled(false);
+            toggle = !toggle;
+            b1[0][0].setLabel("O");
         }
 
-        if(b[0].getLabel() ==b[1].getLabel() && b[1].getLabel()==b[2].getLabel()) {
+        if(b1[0][1] == e.getSource() && toggle && b1[0][1].getLabel().equals("")) {
+            numOfClicks++;
+            toggle = !toggle;
+            b1[0][1].setLabel("X");
+        }
+        else if(b1[0][1] == e.getSource() && !toggle && b1[0][1].getLabel().equals("")) {
+            numOfClicks++;
+            toggle = !toggle;
+            b1[0][1].setLabel("O");
+        }
+
+        if(b1[0][2] == e.getSource() && toggle && b1[0][2].getLabel().equals("")) {
+            numOfClicks++;
+            toggle = !toggle;
+            b1[0][2].setLabel("X");
+        }
+        else if(b1[0][2] == e.getSource() && !toggle && b1[0][2].getLabel().equals("")) {
+            numOfClicks++;
+            toggle = !toggle;
+            b1[0][2].setLabel("O");
+        }
+
+        if(b1[1][0] == e.getSource() && toggle && b1[1][0].getLabel().equals("")) {
+            numOfClicks++;
+            toggle = !toggle;
+            b1[1][0].setLabel("X");
+        }
+        else if(b1[1][0] == e.getSource() && !toggle && b1[1][0].getLabel().equals("")) {
+            numOfClicks++;
+            toggle = !toggle;
+            b1[1][0].setLabel("O");
+        }
+
+        if(b1[1][1] == e.getSource() && toggle && b1[1][1].getLabel().equals("")) {
+            numOfClicks++;
+            toggle = !toggle;
+            b1[1][1].setLabel("X");
+        }
+        else if(b1[1][1] == e.getSource() && !toggle && b1[1][1].getLabel().equals("")) {
+            numOfClicks++;
+            toggle = !toggle;
+            b1[1][1].setLabel("O");
+        }
+
+        if(b1[1][2] == e.getSource() && toggle && b1[1][2].getLabel().equals("")) {
+            numOfClicks++;
+            toggle = !toggle;
+            b1[1][2].setLabel("X");
+        }
+        else if(b1[1][2] == e.getSource() && !toggle && b1[1][2].getLabel().equals("")) {
+            numOfClicks++;
+            toggle = !toggle;
+            b1[1][2].setLabel("O");
+        }
+
+        if(b1[2][0] == e.getSource() && toggle && b1[2][0].getLabel().equals("")) {
+            numOfClicks++;
+            toggle = !toggle;
+            b1[2][0].setLabel("X");
+        }
+        else if(b1[2][0] == e.getSource() && !toggle && b1[2][0].getLabel().equals("")) {
+            numOfClicks++;
+            toggle = !toggle;
+            b1[2][0].setLabel("O");
+        }
+
+        if(b1[2][1] == e.getSource() && toggle && b1[2][1].getLabel().equals("")) {
+            numOfClicks++;
+            toggle = !toggle;
+            b1[2][1].setLabel("X");
+        }
+        else if(b1[2][1] == e.getSource() && !toggle && b1[2][1].getLabel().equals("")) {
+            numOfClicks++;
+            toggle = !toggle;
+            b1[2][1].setLabel("O");
+        }
+
+        if(b1[2][2] == e.getSource() && toggle && b1[2][2].getLabel().equals("")) {
+            numOfClicks++;
+            toggle = !toggle;
+            b1[2][2].setLabel("X");
+        }
+        else if(b1[2][2] == e.getSource() && !toggle && b1[2][2].getLabel().equals("")) {
+            numOfClicks++;
+            toggle = !toggle;
+            b1[2][2].setLabel("O");
+        }
+
+        if(b1[0][0].getLabel() ==b1[1][1].getLabel() && b1[0][0].getLabel()==b1[2][2].getLabel() && !b1[0][0].getLabel().equals("")) {
+            isWin = true;
+            allow = true;
             repaint();
         }
-        else if(b[0].getLabel() ==b[3].getLabel() && b[3].getLabel()==b[6].getLabel()) {
+        else if(b1[0][0].getLabel() ==b1[0][1].getLabel() && b1[0][0].getLabel()==b1[0][2].getLabel() && !b1[0][0].getLabel().equals("")) {
+            isWin = true;
+            allow = true;
             repaint();
         }
-        else if(b[0].getLabel() ==b[4].getLabel() && b[1].getLabel()==b[8].getLabel()) {
+        else if(b1[0][0].getLabel() ==b1[1][0].getLabel() && b1[1][0].getLabel()==b1[2][0].getLabel() && !b1[0][0].getLabel().equals("")) {
+            isWin = true;
+            allow = true;
             repaint();
         }
-        else if(b[3].getLabel() ==b[4].getLabel() && b[4].getLabel()==b[5].getLabel()) {
+        else if(b1[0][1].getLabel() ==b1[1][1].getLabel() && b1[0][1].getLabel()==b1[2][1].getLabel() && !b1[0][1].getLabel().equals("")) {
+            isWin = true;
+            allow = true;
             repaint();
         }
-        else if(b[1].getLabel() ==b[4].getLabel() && b[4].getLabel()==b[7].getLabel()) {
+        else if(b1[1][1].getLabel() ==b1[1][0].getLabel() && b1[1][1].getLabel()==b1[1][2].getLabel() && !b1[1][1].getLabel().equals("")) {
+            isWin = true;
+            allow = true;
             repaint();
         }
-        else if(b[2].getLabel() ==b[4].getLabel() && b[4].getLabel()==b[6].getLabel()) {
+        else if(b1[2][0].getLabel() ==b1[2][1].getLabel() && b1[2][0].getLabel()==b1[2][2].getLabel() && !b1[2][0].getLabel().equals("")) {
+            isWin = true;
+            allow = true;
             repaint();
         }
-        else if(b[2].getLabel() ==b[5].getLabel() && b[5].getLabel()==b[8].getLabel()) {
+        else if(b1[0][2].getLabel() ==b1[1][1].getLabel() && b1[0][2].getLabel()==b1[2][0].getLabel() && !b1[0][2].getLabel().equals("")) {
+            isWin = true;
+            allow = true;
             repaint();
         }
-        else if(b[6].getLabel() ==b[7].getLabel() && b[7].getLabel()==b[8].getLabel()) {
+        else if(b1[0][2].getLabel() ==b1[1][2].getLabel() && b1[0][2].getLabel()==b1[2][2].getLabel() && !b1[0][2].getLabel().equals("")) {
+            isWin = true;
+            allow = true;
             repaint();
         }
         else  {
             if(numOfClicks==9){
+                isWin = false;
+                allow = true;
                 repaint();
             }
         }
     }
 }
 
+class MyDialog extends Dialog implements WindowListener {
+
+    Panel p;
+    TextField tf;
+
+    public MyDialog(Layout c, String title, boolean model) {
+        super(c, title, model);
+        p = new Panel();
+        tf = new TextField(title);
+        tf.setEditable(false);
+        tf.setEnabled(false);
+        p.add(tf);
+        p.setSize(300,100);
+        add(p);
+        setSize(300,100);
+        setResizable(false);
+        addWindowListener(this);
+    }
+
+    @Override
+    public void windowOpened(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+        this.dispose();
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowActivated(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+
+    }
+}
